f18897362aa96c75eeaac32254999ae2259d8c5d
Pablo Baños López <pbl1986@gmail.com>
2014-02-07 19:44:35 +0100

    Defines specs for GadgetImage

---
+++ spec/models/gadget_image_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe GadgetImage do
+	let(:gadget){create(:gadget)}
+	describe '#valid?' do
+		let(:valid_attributes) do
+			{
+				gadget: gadget,
+				attachment: File.new('/spec/images/image-1.jpg')
+			}
+		end
+		it "returns true given valid attributes" do
+			expect(GadgetImage.new(valid_attributes)).to be_valid
+		end
+		it "returns false without a gadget" do
+			expect(GadgetImage.new(valid_attributes.except(:gadget))).not_to be_valid
+		end
+		it "returns false without an attachment" do
+			expect(GadgetImage.new(valid_attributes.except(:attachment))).not_to be_valid
+		end
+		it "returns false without an image-typed attachment" do
+			expect(GadgetImage.new(valid_attributes.with(attachment: File.new('/spec/images/non-image.txt')))).not_to be_valid
+		end
+	end
+end
