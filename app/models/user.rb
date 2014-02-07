8d01b27d71ca40631284a15d1f38dd33376dbf74
Pablo Baños López <pbl1986@gmail.com>
2014-02-07 14:06:30 +0100

    Makes list gadget scenario pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+	has_many :gadgets
+
 	validates :username, presence: true, uniqueness: true
 	validates :password, presence: true, confirmation: true
 	validates :password_confirmation, presence: {if: :new_record?}
--- features/step_definitions/gadget_steps.rb
+++ features/step_definitions/gadget_steps.rb
@@ -1,28 +1,34 @@
 Given(/^I am authenticated as '([^']+)'$/) do |username|
-  step("a '#{username}' user exists")
+  step("a '#{username}' user exists with password 'Secret.123'")
   user = User.find_by username: username
   step('I am not currently logged in')
   step('I go to the login page')
   step("I fill in 'Username' with \"#{user.username}\"")
-  step("I fill in 'Password' with \"#{user.password}\"")
+  step("I fill in 'Password' with \"Secret.123\"")
   step('I press "Log in"')
 end
 
 Given(/^the user "(.*?)" has as gadgets$/) do |username, table|
-  # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
+  step("a '#{username}' user exists")
+  user = User.find_by username: username
+  Gadget.where(user_id: user.id).destroy_all
+  table.rows.flatten.each do |gadget_name|
+  	Gadget.create(user: user, name: gadget_name)
+  end
 end
 
 When(/^I go to the user page$/) do
-  pending # express the regexp above with the code you wish you had
+  visit '/'
 end
 
 Then(/^I should see the gadgets$/) do |table|
-  # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
+  table.rows.flatten.each do |gadget_name|
+  	page.should have_content(gadget_name)
+  end
 end
 
 Then(/^I should not see the gadgets$/) do |table|
-  # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
+  table.rows.flatten.each do |gadget_name|
+  	page.should_not have_content(gadget_name)
+  end
 end
