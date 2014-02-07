05c6da7c5ff0acd51cbb451a98528a8a0dde0e7a
Pablo Baños López <pbl1986@gmail.com>
2014-02-07 17:00:40 +0100

    Makes Gadget.search specs pass

--- app/models/gadget.rb
+++ app/models/gadget.rb
@@ -4,4 +4,8 @@ class Gadget < ActiveRecord::Base
 
 	validates :name, presence: true, uniqueness: {scope: :user_id}
 	validates :user_id, presence: true
+
+	def self.search(user, query)
+		where("user_id = ? AND name LIKE ?", user.id, "%#{query}%")
+	end
 end
