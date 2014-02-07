c9ea8bc6663acab39ce4c064aade498494892ae8
Pablo Baños López <pbl1986@gmail.com>
2014-02-07 17:41:27 +0100

    Refactoring: removes unused db column password in users table and simplifies model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ActiveRecord::Base
 	has_many :gadgets
 
 	validates :username, presence: true, uniqueness: true
-	validates :password, presence: true, confirmation: true
 	validates :password_confirmation, presence: {if: :new_record?}
 
 	devise :database_authenticatable, :registerable, :validatable
